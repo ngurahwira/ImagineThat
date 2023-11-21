25055611272e35573cb86fdc8f43df0d8cf85b3d
Ngurah Wira <ngurahwira27@gmail.com>
2023-11-21 22:41:54 +0700

    feat : message room

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+// Mengatur CORS pada Socket.IO
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("Message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Listening on :3000");
+});
