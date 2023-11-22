92c0d8f6f2c9a60a4301098684aa4eca250aff31
RafiNaufl <nrafialdhawi@gmail.com>
2023-11-22 14:35:16 +0700

    update tampilan home baru setengah

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import MessageList from "./MessageList";
 import MessageInput from "./MessageInput";
 
 const ChatRoom = () => {
   const name = localStorage.getItem("name") || "Anonymous";
+
   return (
-    <div>
-      <MessageList />
-      <MessageInput name={name} />
-    </div>
+    <Card
+      style={{
+        width: "100%",
+        maxWidth: "600px",
+        margin: "auto",
+        marginTop: "100px",
+      }}
+    >
+      <Card.Header as="h5">Chat Room</Card.Header>
+      <Card.Body style={{ height: "365px", overflowY: "scroll" }}>
+        <MessageList />
+      </Card.Body>
+      <Card.Footer>
+        <MessageInput name={name} />
+      </Card.Footer>
+    </Card>
   );
 };
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,18 +1,38 @@
 import ChatRoom from "../components/ChatRoom";
 import WhiteBoard from "../components/WhiteBoard";
+import { Container } from "react-bootstrap";
+import backgroundImage from "../assets/bg1.jpg";
 
 const HomePage = () => {
   const name = localStorage.getItem("name") || "Anonymous";
   return (
-    <>
+    <Container
+      fluid
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        padding: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "stretch",
+      }}
+    >
       <div className="App">
         <h2>Welcome, {name}</h2>
-        <header className="App-header">
-          <WhiteBoard />
-          <ChatRoom />
+        <header
+          className="App-header"
+          style={{ display: "flex", width: "100%", gap: "40px" }}
+        >
+          <WhiteBoard style={{ flex: 1 }} />
+          <ChatRoom style={{ flex: 1 }} />
         </header>
       </div>
-    </>
+    </Container>
   );
 };
 
