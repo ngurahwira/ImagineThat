df8505dd4ba747cf8fd878e8a74359b910af7bce
Ngurah Wira <ngurahwira27@gmail.com>
2023-11-22 16:22:05 +0700

    implement redux in MessageList.jsx

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "../app/store";
 import MessageList from "./MessageList";
 import MessageInput from "./MessageInput";
 
 const ChatRoom = () => {
   const name = localStorage.getItem("name") || "Anonymous";
   return (
-    <div>
-      <MessageList />
-      <MessageInput name={name} />
-    </div>
+    <Provider store={store}>
+      <div>
+        <MessageList />
+        <MessageInput name={name} />
+      </div>
+    </Provider>
   );
 };
 
